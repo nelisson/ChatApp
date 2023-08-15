b318f770b464909ce6202ccfd06e16de5cd0cc58
nelisson <nelisson.ad@gmail.com>
2023-08-15 08:35:17 -0300

    refac: add required for some fields in Chatroom and Message

--- ChatApp/Shared/Model/Chatroom.cs
+++ ChatApp/Shared/Model/Chatroom.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChatApp.Shared.Model
+﻿namespace ChatApp.Shared.Model
 {
     public class Chatroom
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public virtual ICollection<Message> Messages { get; set; }
+        public required string Name { get; set; }
+        public virtual required List<Message> Messages { get; set; }
     }
 }
--- ChatApp/Shared/Model/Message.cs
+++ ChatApp/Shared/Model/Message.cs
@@ -1,20 +1,14 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChatApp.Shared.Model
 {
     public class Message
     {
         public int Id { get; set; }
-        public string UserId { get; set; }
-        public virtual IdentityUser User { get; set; }
+        public required string UserId { get; set; }
         public DateTime Timestamp { get; set; }
-        public string Content { get; set; }
+        public required string Content { get; set; }
         public int ChatroomId { get; set; }
-        public virtual Chatroom Chatroom { get; set; }
+        public required virtual Chatroom Chatroom { get; set; }
     }
 }
